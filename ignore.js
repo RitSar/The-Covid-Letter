88dc10730a2c9a17e17ee5e842ea8c52156a4388
RitSar <55022926+RitSar@users.noreply.github.com>
2020-04-20 22:25:34 +0530

    gitignore made an error for heroku obviously as it didnt find ignore.js

---
+++ ignore.js
@@ -0,0 +1,3 @@
+module.exports.rapidapikey = "cfaa20c42bmshbce894d2f056cedp1f2134jsnb73068c89b0d";
+module.exports.rapidapikeys = "ee804b6db0msh1104c691236b926p1215d6jsncdfef605c51a";
+module.exports.mailchimpauth = "rittik:9762d78ae886bc4931b126ae87b3e1d3-us19";
